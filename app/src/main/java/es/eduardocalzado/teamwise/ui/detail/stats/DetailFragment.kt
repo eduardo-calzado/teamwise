8523b09364d52d31eaaa4fc8e1f2b55d63f31100
Eduardo Calzado <eduardo.calzado.roura@gmail.com>
2022-07-12 21:16:24 +0200

    fix(model): wip

--- app/src/main/java/es/eduardocalzado/teamwise/ui/detail/stats/DetailFragment.kt
+++ app/src/main/java/es/eduardocalzado/teamwise/ui/detail/stats/DetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -16,7 +17,7 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class DetailFragment : Fragment(R.layout.fragment_detail) {
 
-    private val viewModel: DetailViewModel by activityViewModels()
+    private val viewModel: DetailViewModel by viewModels()
 
     private lateinit var detailState: DetailState
 
