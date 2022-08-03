6dbccdc0b2454fb120f9123c4a581729d879adb4
Eduardo Calzado <eduardo.calzado.roura@gmail.com>
2022-08-03 16:55:52 +0200

    fix(me): add the progressbar

--- app/src/main/java/es/eduardocalzado/teamwise/ui/main/me/MeFragment.kt
+++ app/src/main/java/es/eduardocalzado/teamwise/ui/main/me/MeFragment.kt
@@ -2,6 +2,7 @@ package es.eduardocalzado.teamwise.ui.main.me
 
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,7 +20,13 @@ class MeFragment : Fragment(R.layout.fragment_me) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentMeBinding.bind(view).apply {
-            meWebview.webViewClient = WebViewClient()
+            meWebview.webViewClient = object: WebViewClient() {
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    binding.meWebview.visibility = View.VISIBLE
+                    binding.progressbar.visibility = View.GONE
+                }
+            }
             meWebview.loadUrl(Constants.EDUARDOCALZADO_ES)
             meWebview.settings.javaScriptEnabled = true
             meWebview.settings.setSupportZoom(false)
